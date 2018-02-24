3e5a852fd67f2d2dbb2c42191a3ab3b641902fe1
Brian <brian1999lin@gmail.com>
2018-02-24 13:30:21 -0500

    broken

--- public/js/firebase-login.js
+++ public/js/firebase-login.js
@@ -1,4 +1,5 @@
 var provider = new firebase.auth.GoogleAuthProvider();
+var database = firebase.database();
 
 function signin(){
 	firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -19,7 +20,16 @@ function signin(){
 });
 }
 
+
+var user = firebase.auth().currentUser;
+
+function writeTest(){
+  writeUserData(document.getElementById("username-input").value, document.getElementById("password-input").value, firebase.auth().currentUser;);
+  console.log("hi i did this");
+}
 function writeUserData(userId, name, password) {
+  console.log(name);
+  console.log(password);
   firebase.database().ref('users/' + userId).set({
     username: name,
     password: password,
