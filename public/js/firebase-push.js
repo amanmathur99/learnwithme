38c402c01026edff16716521a099a36decd5dcc5
Brian <brian1999lin@gmail.com>
2018-02-24 15:23:18 -0500

    fetch events

--- public/js/firebase-push.js
+++ public/js/firebase-push.js
@@ -25,3 +25,21 @@ function writeData(name, details){
 		details: details,
 	});
 }
+function outputEvents(){
+	var eventsRef = firebase.database().ref('events/');
+
+	var events = [];
+
+	var rootRef = firebase.database().ref();
+	var urlRef = rootRef.child("events/");
+	urlRef.once("value", function(snapshot) {
+	  snapshot.forEach(function(child) {
+	  	indEventArr = [];
+	    child.forEach(function(child){
+	    	indEventArr.push(child.val());
+	    });
+	    events.push(indEventArr);
+	  });
+	});
+	return events;
+}
