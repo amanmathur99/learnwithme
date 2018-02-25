a0210b51c4f7097e99a618f4cc65c4a11f336e6c
brian1999lin <brian1999lin@gmail.com>
2018-02-24 19:19:24 -0500

    update

---
+++ public/js/event-populate.js
@@ -0,0 +1,20 @@
+function outputEvents(callback){
+	var eventsRef = firebase.database().ref('events/');
+
+	var events = [];
+
+	var rootRef = firebase.database().ref();
+	var urlRef = rootRef.child("events/");
+	
+	urlRef.once("value", function(snapshot) {
+	  snapshot.forEach(function(child) {
+	  	indEventArr = [];
+	    child.forEach(function(child){
+	    	indEventArr.push(child.val());
+	    });
+	    events.push(indEventArr);
+	  });
+	  callback(events);
+	});
+	
+}
--- public/js/firebase-push.js
+++ public/js/firebase-push.js
@@ -32,23 +32,3 @@ function writeData(name, description, time, location, image){
 		eventimage: image,
 	});
 }
-function outputEvents(callback){
-	var eventsRef = firebase.database().ref('events/');
-
-	var events = [];
-
-	var rootRef = firebase.database().ref();
-	var urlRef = rootRef.child("events/");
-	
-	urlRef.once("value", function(snapshot) {
-	  snapshot.forEach(function(child) {
-	  	indEventArr = [];
-	    child.forEach(function(child){
-	    	indEventArr.push(child.val());
-	    });
-	    events.push(indEventArr);
-	  });
-	  callback(events);
-	});
-	
-}
